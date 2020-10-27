814a84624d61c3274266adf7059cdeeec9b2c9d0
yoshihiro-kawamura <73100863+yoshihiro-kawamura@users.noreply.github.com>
2020-10-27 20:56:29 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,14 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    @message.save
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
   end
+
 
   private
 
